e290df0ca13eeffd50826649745d2d183fe7fc42
Sumit Pandey <skpandey12941@gmail.com>
2020-05-13 09:27:12 +0530

    removed callback hell

--- src/components/Posts/userPost.jsx
+++ src/components/Posts/userPost.jsx
@@ -25,16 +25,18 @@ class UserPost extends Component {
 
     componentDidMount() {
 
-        axios(`https://jsonplaceholder.typicode.com/posts/${this.props.match.params.postId}`)
-        .then(post => {
-            console.log(post)
+       const postResponse = axios(`https://jsonplaceholder.typicode.com/posts/${this.props.match.params.postId}`).then(post => {
             this.setState((state) => {
                return {
                 post: post.data
                 }  
             })
+        })
+        .catch(error => {
+            throw new Error(error)
+        })
 
-            axios(`https://jsonplaceholder.typicode.com/users/${this.state.post.userId}`)
+         postResponse.then(() => {   axios(`https://jsonplaceholder.typicode.com/users/${this.state.post.userId}`)
         .then(user => {
             this.setState({
                 username: user.data.username,
@@ -44,73 +46,78 @@ class UserPost extends Component {
         .catch(error => {
             throw new Error(error)
         })
+    })
 
-        axios(`https://jsonplaceholder.typicode.com/posts/${this.props.match.params.postId}/comments`)
+    postResponse.then(() => { axios(`https://jsonplaceholder.typicode.com/posts/${this.props.match.params.postId}/comments`)
         .then(comment => {
-            console.log(comment)
             this.setState({
                 comment: comment.data,
                 isLoaded2: true
             })
         })
-        .catch(error => {
-            console.log(error)
-            throw new Error(error)
-        })
         })
         .catch(error => {
-            console.log(error)
             throw new Error(error)
         })
-
-        
-
-      
     }
+        
     
     render() {
         const { post, username, comment, isLoaded1, isLoaded2 } = this.state 
 
         const comments = comment.map(comment => {
             return(
-                <Typography id={comment.id} gutterBottom component="p">
-                <span id={comment.id}><b>comment by </b>  {comment.name}: {comment.body}</span>
+                <Typography key={comment.id} id={comment.id} gutterBottom component="p">
+                <span id={comment.id}><b>comment by </b> <u><i> {comment.name} </i></u>: {comment.body}</span>
                 </Typography>
                 )
         })  
 
-        const userPost = (<React.Fragment>
-            <div><h3>UserPost</h3></div>
+        const userPost = (
+        <React.Fragment>
+            <div>
+                <h3>UserPost</h3>
+            </div>
+
             <div key={post.id} style={styles._div}>
-            <Card key={post.id} id={post.id} style={styles._card}>
-            <CardActionArea id={post.id}>
-              <CardContent id={post.id}>
-    
-                <Typography id={post.id} gutterBottom component="p">
-                <span id={post.id}><b>UserId</b>: {post.userId} </span>
-                </Typography>
 
-                <Typography id={post.id} gutterBottom component="p">
-                <span id={post.id}><b>UserName</b>: {username} </span>
-                </Typography>
-    
-                <Typography id={post.id} gutterBottom component="p">
-                <span id={post.id}><b>Title</b>: {post.title} </span>
-                </Typography>
+                <Card key={post.id} id={post.id} style={styles._card}>
 
-                <Typography id={post.id} gutterBottom component="p">
-                <span id={post.id}><b>Body</b>: {post.body} </span>
-                </Typography>
+                    <CardActionArea id={post.id}>
 
-                <React.Fragment>{comments}</React.Fragment>
+                        <CardContent id={post.id}>
+
+                            <Typography id={post.id} gutterBottom component="p">
+                                <span id={post.id}><b>UserId</b>: {post.userId} </span>
+                            </Typography>
+                            
+                            <Typography id={post.id} gutterBottom component="p">
+                                <span id={post.id}><b>UserName</b>: {username} </span>
+                            </Typography>
     
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          </div>
-          </React.Fragment>)
+                            <Typography id={post.id} gutterBottom component="p">
+                            <span id={post.id}><b>Title</b>: {post.title} </span>
+                            </Typography>
+
+                            <Typography id={post.id} gutterBottom component="p">
+                            <span id={post.id}><b>Body</b>: {post.body} </span>
+                            </Typography>
+
+                            <React.Fragment>{comments}</React.Fragment>
+    
+                        </CardContent>
+
+                    </CardActionArea>
+
+                </Card>
+
+            </div>
+
+        </React.Fragment>
+        )
+
         return (
-            ( (isLoaded1 === false || isLoaded2 === false) ? <div> Loading... </div> : <div> {userPost} </div> )
+            ( ( isLoaded1 === false || isLoaded2 === false ) ? <div> Loading... </div> : <div> {userPost} </div> )
         )
         
     }
